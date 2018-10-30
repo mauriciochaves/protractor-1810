867d711ee8c2db6093727e26fbe2bef0c4c6702e
Fernando Papito <eu@papito.io>
2018-10-29 21:32:31 -0300

    ajustando o projeto para testes em headless com Selenium Server

--- conf.js
+++ conf.js
@@ -1,7 +1,8 @@
 // Arquivo de configuração do Protractor
 
 exports.config = {
-    directConnect: true,
+    // directConnect: true,
+    seleniumAddress: 'http://selenium_server:4444/wd/hub',
     framework: 'jasmine2',
     specs: ['specs/**-spec.js'],
     baseUrl: 'https://mark7.herokuapp.com',
